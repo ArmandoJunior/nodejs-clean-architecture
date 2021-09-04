d6eae57a3d4c4145a18c5e7c9845a3d195a47955
Armando N. Junior <armando.n@futebolcard.com.br>
2021-09-04 15:32:37 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+};
