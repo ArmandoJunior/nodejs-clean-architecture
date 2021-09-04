a85bd5563ad5dd5f048ce778f77adbe785c17bdc
Armando N. Junior <armando.n@futebolcard.com.br>
2021-09-04 17:59:02 -0300

    move sut creation to a factory helper method

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,9 +1,13 @@
 const MissingParamError = require('../helpers/missing-param-error.js')
 const LoginRouter = require('./login-router.js')
 
+const makeSut = () => {
+  return new LoginRouter()
+}
+
 describe('Login Router', () => {
   test('Should return 400 if no email is privided', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -15,7 +19,7 @@ describe('Login Router', () => {
   })
 
   test('Should return 400 if no password is privided', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com'
@@ -27,13 +31,13 @@ describe('Login Router', () => {
   })
 
   test('Should return 500 if no httpResquest is privided', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
   })
 
   test('Should return 500 if httpResquest has no body', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {}
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
