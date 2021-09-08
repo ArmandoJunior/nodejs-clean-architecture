17354cc2c02ec52e6cc1a3dcc7d252d4e9fbffa6
Armando N. Junior <armando.n@futebolcard.com.br>
2021-09-08 12:38:06 -0300

    ensure EmailValidator return false if validator return false

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -1,13 +1,26 @@
+const validator = require('validator')
 class EmailValidator {
-  isValid(email) {
-    return true
+  isValid (email) {
+    return validator.isEmail(email)
   }
 }
 
 describe('email validator', () => {
+  /**
+   * Right now, I'm going to add a new lib with the following command:
+   *
+   * npm i validator
+   */
   test('Should return true if validator return true', () => {
     const sut = new EmailValidator()
     const emailIsValid = sut.isValid('valid_email@email.com')
     expect(emailIsValid).toBe(true)
+  })
+
+  test('Should return false if validator return false', () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const emailIsValid = sut.isValid('invalid_email#email.com')
+    expect(emailIsValid).toBe(false)
   })
 })
